9693557cec238c5bf74f8794f15f0db23fad2b04
Alexander <alex_f_k@live.ru>
2017-04-01 11:21:58 +0300

    Сортировка вставками
    Ярослав 03.31.17

--- KSUTeamProject/KSUTeamProject/Sort Methods/08-InsertSort.cs
+++ KSUTeamProject/KSUTeamProject/Sort Methods/08-InsertSort.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 
 namespace KSUTeamProject {
-	class _8_InsertSort {
-		public static void Sort(ref int[] input) {
-
+	class _8_InsertSort 
+		public static void Sort(ref int[] mas)  
+		{
+			int key,nomsim;
+			for (int i = 1; i < mas.Length; i++)  //i=1???
+			{
+				key = mas[i];          
+				nomsim = i-1;        
+				while (nomsim > -1 && mas[nomsim] > key)      
+				{ 
+					mas[nomsim + 1] = mas[nomsim];       
+					nomsim--;   
+				}
+				mas[nomsim + 1] = key;
+			}
 		}
 	}
 }
